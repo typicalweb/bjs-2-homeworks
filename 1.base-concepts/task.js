098d7463ab2d6e3741422c0a9636d8854897c6a9
education-2002 <education-2002@mail.ru>
2024-02-25 13:35:49 +0900

    Первое и второе задания полностью готовы

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
-"use strict"
+// Домашнее задание к лекции 1 «Основные понятия»
+
+"use strict";
+// Задача 1
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  const d = b * b - 4 * a * c;
+  if (d < 0) {
+    return [];
+  } else if (d === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
+  return arr; // array
 }
 
+
+// Задача 2
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyPercentage = ((percent/100)/12);
+  let loanBody = (amount-contribution);
+  let montlyPayment = (loanBody*(monthlyPercentage+(monthlyPercentage/(((1+monthlyPercentage)**countMonths)-1))));
+  let totalAmount = (montlyPayment*countMonths);
+
+  return +(totalAmount.toFixed(2));
 }
+
+console.log(calculateTotalMortgage(10, 0, 20000, 24.));
+
